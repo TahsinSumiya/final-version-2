34c2221b928d43ded0c1a74a767e20eaef087f0f
Tahsin Sumiya <Tahsinadity187@gmail.com>
2023-12-20 20:58:57 +0600

    working

--- frontend/src/component/Admin/DeleteLayoutByAdmin.js
+++ frontend/src/component/Admin/DeleteLayoutByAdmin.js
@@ -1,44 +1,51 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-export default function DeleteLayoutByAdmin({layoutId,email}) {
-    const [emailSent, setEmailSent] = useState(false);
-    const sendEmail = (e) => {
-   
-   
-      try {
-        const response =  axios.post('http://localhost:80/api/autoemail', {
-          userEmail: email
-        });
-  
-        if (response.status === 201) {
-          setEmailSent(true);
-        } else {
-          console.error('Email not sent.');
-        }
-      } catch (error) {
-        console.error('Error sending email:', error);
-      }
-    };
-      const handleDelete = () => {
-        
-          axios.delete(`http://localhost:80/api/layouts/${layoutId}`)
-            .then(response => {
-              alert('deleted succesfully')
-              console.log(response.data.message);
-              sendEmail() // Layout deleted successfully message
-              // You may want to refresh the layouts after deletion
-              // You can use state management libraries like Redux for this purpose
-            })
-            .catch(error => {
-              alert(error)
-              console.error('Error deleting layout:', error);
-            });
-        };
-  return (
-    
-       <div>
-       <buttpn class="relative my-5 text-red-600 cursor-pointer" onClick={handleDelete}><i class="bi bi-trash3"></i></buttpn>
-    </div>
 
-  )
+export default function DeleteLayoutByAdmin({ layoutId, email }) {
+  const [emailSent, setEmailSent] = useState(false);
+
+  const sendEmail = () => {
+    try {
+      if (!email) {
+        console.error('No email provided. Cannot send email.');
+        return;
+      }
+
+      const response = axios.post('http://localhost:80/api/autoemail', {
+        userEmail: email,
+      });
+
+      if (response.status === 201) {
+        setEmailSent(true);
+      } else {
+        console.error('Email not sent.');
+      }
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
+  };
+
+  const handleDelete = () => {
+    axios
+      .delete(`http://localhost:80/api/layouts/${layoutId}`)
+      .then((response) => {
+        alert('Deleted successfully');
+        console.log(response.data.message);
+        sendEmail(); // Layout deleted successfully message
+        // You may want to refresh the layouts after deletion
+        // You can use state management libraries like Redux for this purpose
+      })
+      .catch((error) => {
+        alert(error);
+        console.error('Error deleting layout:', error);
+      });
+  };
+
+  return (
+    <div>
+      <button class="relative my-5 text-red-600 cursor-pointer" onClick={handleDelete}>
+        <i class="bi bi-trash3"></i>
+      </button>
+    </div>
+  );
 }
